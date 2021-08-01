2e4d706ad11ef884903f9cf11394b71934bf4437
Mateus Nadal <nadalmateus@hotmail.com>
2021-08-01 17:41:57 -0300

    arrays

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace csharpArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var meuArray = new int[5];
+
+            meuArray[0] = 10;
+            Console.WriteLine(meuArray[0]);
+
+
+        }
+    }
+}
